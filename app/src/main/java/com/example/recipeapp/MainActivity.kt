fdb51219d00ed317c1a2462810e7c5246ca805d0
Aappo <93522510+Olvix@users.noreply.github.com>
2023-03-27 19:43:51 +0300

    Recipe opening onClick listener added

--- app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.recipeapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.textView)
+
+        textView.setOnClickListener {
+            val intent = Intent(this, RecipeActivity::class.java)
+            intent.putExtra("recipe_name", "Herkullinen tonnikala pasta")
+            startActivity(intent)
+        }
     }
 }
