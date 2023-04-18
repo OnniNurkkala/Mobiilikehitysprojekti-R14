63552f0772ed392d865e9fbcc4f114371a1e355a
Aappo <93522510+Olvix@users.noreply.github.com>
2023-04-18 21:41:12 +0300

    Added comments to my components

--- app/src/main/java/com/example/recipeapp/RecipeActivity.kt
+++ app/src/main/java/com/example/recipeapp/RecipeActivity.kt
@@ -9,14 +9,17 @@ class RecipeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recipe)
 
+        // Get references to the views in the activity
         val recipeNameTextView = findViewById<TextView>(R.id.recipe_name)
         val recipeIngredientsTextView = findViewById<TextView>(R.id.recipe_ingredients)
         val recipeStepsTextView = findViewById<TextView>(R.id.recipe_steps)
 
+        // Get the recipe name, ingredients, and steps from the intent that started this activity
         val recipeName = intent.getStringExtra("recipe_name")
         val recipeIngredients = intent.getStringExtra("recipe_ingredients")
         val recipeSteps = intent.getStringExtra("recipe_steps")
 
+        // Set the text of the views to the recipe name, ingredients, and steps
         recipeNameTextView.text = recipeName
         recipeIngredientsTextView.text = recipeIngredients
         recipeStepsTextView.text = recipeSteps
--- app/src/main/java/com/example/recipeapp/RecipeIntentFactory.kt
+++ app/src/main/java/com/example/recipeapp/RecipeIntentFactory.kt
@@ -3,11 +3,17 @@ package com.example.recipeapp
 import android.content.Context
 import android.content.Intent
 
+// This is a companion object, which is similar to a static method in Java
+// It allows you to define methods that can be called on the class itself, rather than an instance of the class
 class RecipeIntentFactory {
     companion object {
+        // This is a method that creates and returns an Intent object for a recipe activity
         fun createIntent(context: Context, recipeName: String): Intent {
+            // Create a new Intent object, specifying the context and the class of the activity to launch
             val intent = Intent(context, RecipeActivity::class.java)
+            // Add the recipe name as an extra to the Intent object
             intent.putExtra("recipe_name", recipeName)
+            // Return the Intent object
             return intent
         }
     }
