0d4dd1f8b38310cfcda8735719eb7c9b2b745d07
Aappo <93522510+Olvix@users.noreply.github.com>
2023-04-18 21:37:32 +0300

    Fixed intents on recipes

--- app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -26,6 +26,49 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Find the TextView elements in the layout and store them in variables
+        val tunaText = findViewById<TextView>(R.id.tunaText)
+        val nachoText = findViewById<TextView>(R.id.nachoText)
+        val noodleText = findViewById<TextView>(R.id.noodleText)
+        val omeletteText = findViewById<TextView>(R.id.omeletteText)
+
+        // Set click listeners for each TextView
+        tunaText.setOnClickListener {
+            // Create a new intent using a factory method and pass in the recipe name, ingredients, and steps as extras
+            val intent = RecipeIntentFactory.createIntent(this, "Herkullinen tonnikala pasta")
+            intent.putExtra("recipe_ingredients", getString(R.string.recipe_tuna_ingredients))
+            intent.putExtra("recipe_steps", getString(R.string.recipe_tuna_steps))
+            // Start the RecipeActivity with the new intent
+            startActivity(intent)
+        }
+
+        nachoText.setOnClickListener {
+            // Create a new intent using a factory method and pass in the recipe name, ingredients, and steps as extras
+            val intent = RecipeIntentFactory.createIntent(this, "Jauheliha-nachopaistos")
+            intent.putExtra("recipe_ingredients", getString(R.string.recipe_nacho_ingredients))
+            intent.putExtra("recipe_steps", getString(R.string.recipe_nacho_steps))
+            // Start the RecipeActivity with the new intent
+            startActivity(intent)
+        }
+
+        noodleText.setOnClickListener {
+            // Create a new intent using a factory method and pass in the recipe name, ingredients, and steps as extras
+            val intent = RecipeIntentFactory.createIntent(this, "Broileri-nuudelikeitto")
+            intent.putExtra("recipe_ingredients", getString(R.string.recipe_broiler_ingredients))
+            intent.putExtra("recipe_steps", getString(R.string.recipe_broiler_steps))
+            // Start the RecipeActivity with the new intent
+            startActivity(intent)
+        }
+
+        omeletteText.setOnClickListener {
+            // Create a new intent using a factory method and pass in the recipe name, ingredients, and steps as extras
+            val intent = RecipeIntentFactory.createIntent(this, "Ruokaisa munakas")
+            intent.putExtra("recipe_ingredients", getString(R.string.recipe_omelette_ingredients))
+            intent.putExtra("recipe_steps", getString(R.string.recipe_omelette_steps))
+            // Start the RecipeActivity with the new intent
+            startActivity(intent)
+        }
+
         //delete function
         if (intent?.action == "DELETE_RECORD") {
             val intValue = intent.getIntExtra("int_value", 0)
@@ -40,7 +83,6 @@ class MainActivity : AppCompatActivity() {
         var homeButton: ImageButton = findViewById(R.id.homeButton)
         var favoriteButton: ImageButton = findViewById(R.id.favoriteButton)
         var profileButton: ImageButton = findViewById(R.id.profileButton)
-
 
         //Popup menu definitions
         val inflater = getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
@@ -100,38 +142,6 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, ProfileActivity::class.java).apply {
             }
             startActivity(intent);
-        val tunaText = findViewById<TextView>(R.id.tunaText)
-        val nachoText = findViewById<TextView>(R.id.nachoText)
-        val noodleText = findViewById<TextView>(R.id.noodleText)
-        val omeletteText = findViewById<TextView>(R.id.omeletteText)
-
-        tunaText.setOnClickListener {
-            val intent = RecipeIntentFactory.createIntent(this, "Herkullinen tonnikala pasta")
-            intent.putExtra("recipe_ingredients", getString(R.string.recipe_tuna_ingredients))
-            intent.putExtra("recipe_steps", getString(R.string.recipe_tuna_steps))
-            startActivity(intent)
-        }
-
-        nachoText.setOnClickListener {
-            val intent = RecipeIntentFactory.createIntent(this, "Jauheliha-nachopaistos")
-            intent.putExtra("recipe_ingredients", getString(R.string.recipe_nacho_ingredients))
-            intent.putExtra("recipe_steps", getString(R.string.recipe_nacho_steps))
-            startActivity(intent)
-        }
-
-        noodleText.setOnClickListener {
-            val intent = RecipeIntentFactory.createIntent(this, "Broileri-nuudelikeitto")
-            intent.putExtra("recipe_ingredients", getString(R.string.recipe_broiler_ingredients))
-            intent.putExtra("recipe_steps", getString(R.string.recipe_broiler_steps))
-            startActivity(intent)
-        }
-
-        omeletteText.setOnClickListener {
-            val intent = RecipeIntentFactory.createIntent(this, "Ruokaisa munakas")
-            intent.putExtra("recipe_ingredients", getString(R.string.recipe_omelette_ingredients))
-            intent.putExtra("recipe_steps", getString(R.string.recipe_omelette_steps))
-            startActivity(intent)
-        }
     }
 
     //Inflate the hamburger menu
