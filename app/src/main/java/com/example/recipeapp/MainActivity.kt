97bd378f23ae71c85e538eaf92afab673773cc09
Miiapetri <101567276+Miiapetri@users.noreply.github.com>
2023-04-18 15:11:43 +0300

    add

--- app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -137,7 +137,7 @@ class MainActivity : AppCompatActivity() {
         if (recipeName.trim()!="" && recipe_ing.trim()!="" && recipe_inst.trim()!=""){
             val status = databaseHandler.addRecipe(RecipeClass(recipeName,recipe_ing,recipe_inst))
             if (status > -1){
-                Toast.makeText(applicationContext, "Saved!", Toast.LENGTH_SHORT).show()
+                Toast.makeText(applicationContext, "Tallennettu", Toast.LENGTH_SHORT).show()
             }
         }else {
             Toast.makeText(applicationContext, "Texts can't be blank!", Toast.LENGTH_SHORT).show()
@@ -148,10 +148,10 @@ class MainActivity : AppCompatActivity() {
         val databaseHandler: DatabaseHandler = DatabaseHandler(this)
         val status = databaseHandler.deleteUser(userID)
         if(status > -1) {
-            Toast.makeText(applicationContext, "Record deleted successfully!", Toast.LENGTH_SHORT).show()
+            Toast.makeText(applicationContext, "Poistettu!", Toast.LENGTH_SHORT).show()
             IdDelete.text.clear()
         } else {
-            Toast.makeText(applicationContext, "Error deleting record!", Toast.LENGTH_SHORT).show()
+            Toast.makeText(applicationContext, "Virhe tapahtui!", Toast.LENGTH_SHORT).show()
         }
     }
 }
